3b7b9a9eb4fe469c4826ff2309eb86648b1fde29
Niklas Schikora <schikora@dasachtegebot.de>
2021-11-25 12:01:19 +0100

    Changed footer

--- src/components/ui/Footer.svelte
+++ src/components/ui/Footer.svelte
@@ -3,8 +3,12 @@
     <hr />
     <p>
       <strong>Demo Configurator</strong> by Niklas Schikora. This page uses
-      images by <a href="https://www.tesla.com/">Tesla Motors</a>. All images
-      are courtesy of <a href="https://www.tesla.com/">Tesla Motors</a>
+      images made with a 3D-Model by
+      <a
+        href="https://sketchfab.com/3d-models/tesla-2018-model-3-5ef9b845aaf44203b6d04e2c677e444f"
+        >Ameer Studio</a
+      >. Model is based on products by Tesla Motors and uses their Logo. Those images / logos are courtesy of
+      <a href="https://www.tesla.com/">Tesla Motors</a>
     </p>
   </div>
 </footer>
